80cbc9c4f9434feb9a226ff6b326b49140dbb024
Renan Pinho <pinhorenan@icloud.com>
2023-02-28 22:26:58 -0300

    teste

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+app_name = 'home'
+
 urlpatterns = [
-    path('', views.landing_page),
-    path('home/', views.home),
-    path('login/', views.login),
-    path('contato/', views.contact),
-    path('sobre/', views.about),
+    path('', views.landing_page, name='main'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('contato/', views.contact, name='contato'),
+    path('sobre/', views.about, name='sobre'),
 ]
